d84ed35058e3c8acf044bebe61ecbf6e2f8c8a97
Yohan <gentlebae@gmail.com>
2020-12-29 11:07:45 -0500

    API Improved

--- routes/email.js
+++ routes/email.js
@@ -11,16 +11,24 @@ router.route('/').post((req, res) => {
     const email = req.body.email;
     const nickname = req.body.nickname;
     const transactionId = req.body.transactionId;
-    const description = req.body.description;
+    const tradeItem = req.body.tradeItem;
     if(verify === verifyCheck){
         const sgMail = require('@sendgrid/mail')
         sgMail.setApiKey(process.env.SENDGRID)
         const msg = {
             to: email,
-            from:'gentlebae@gmail.com',
-            subject:'[K-Trade] Trade Request Arrived!',
-            text: 'Trade request arrived regarding following item',
-            html:`<strong>${description}</strong><br /><a href="/${transactionId}>See the Transaction</a>`
+            from:'kfreetrade@gmail.com',
+            subject:`[Kpop Free Trade] Trade Request arrived from ${nickname}!`,
+            text: `Trade request arrived regarding following item: ${tradeItem}`,
+            html:`
+              Trade request received from ${nickname} regarding this item: <strong>${tradeItem}</strong>
+              <br /><br />
+              <a clicktracking=off href="https://kexchange.netlify.app/transaction/${transactionId}">REPLY</a>
+              <br /><br />
+              Trade KPOP Goods!<br />
+              Free&Secure<br />
+              https://kexchange.netlify.app
+              `
         }
         
         sgMail.send(msg)
@@ -39,18 +47,22 @@ router.route('/').post((req, res) => {
 router.route('/feedback').post((req, res) => {
   const verify = req.body.verifycode;
   const verifyCheck = process.env.VERIFY_CODE
-  const email = req.body.verifycode;
+  const email = req.body.email;
   const nickname = req.body.nickname;
-  const description = req.body.description;
+  const feedback = req.body.feedback;
+  const title = req.body.title;
   if(verify === verifyCheck){
       const sgMail = require('@sendgrid/mail')
       sgMail.setApiKey(process.env.SENDGRID)
       const msg = {
-          to: 'gentlebae@gmail.com',
-          from: 'gentlebae@gmail.com',
-          subject:'[FEEDBACK] Feedback arrived',
-          text: description,
-          html:`<strong>${description} from ${nickname}, ${email}</string>`
+          to: 'kfreetrade@gmail.com',
+          from: 'kfreetrade@gmail.com',
+          subject:`[FEEDBACK] Feedback arrived from ${email}`,
+          html:`
+            Title: ${title}<br /><br />
+            ${feedback}<br /><br />
+            from ${nickname}, ${email}
+            `
       }
       
       sgMail.send(msg)
